edbfa06b631579ea3fcc33af819b90d4ab50eb2e
alzalok2002 <alzalok2002@gmail.com>
2026-04-21 04:44:41 +0200

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -1,7 +1,6 @@
 #import <UIKit/UIKit.h>
-#import <Foundation/Foundation.h>
 
-// واجهة المنيو البسيطة
+// واجهة المنيو
 @interface MyMenuV : UIView
 @property (nonatomic, strong) UISwitch *sw1;
 @property (nonatomic, strong) UISwitch *sw2;
@@ -22,7 +21,6 @@
         title.textAlignment = NSTextAlignmentCenter;
         [self addSubview:title];
 
-        // زر تفعيل الثبات (Switch 1)
         self.sw1 = [[UISwitch alloc] initWithFrame:CGRectMake(20, 60, 0, 0)];
         [self addSubview:self.sw1];
         
@@ -31,7 +29,6 @@
         l1.textColor = [UIColor whiteColor];
         [self addSubview:l1];
 
-        // زر تفعيل الانتينة (Switch 2)
         self.sw2 = [[UISwitch alloc] initWithFrame:CGRectMake(20, 110, 0, 0)];
         [self addSubview:self.sw2];
 
@@ -44,18 +41,46 @@
 }
 @end
 
-// كود اكتشاف الضغط بـ 3 أصابع
+// كود التحكم في إظهار وإخفاء المنيو
+static MyMenuV *myGlobalMenu;
+
+@interface MenuHandler : NSObject
++ (void)handleTap;
+@end
+
+@implementation MenuHandler
++ (void)handleTap {
+    if (myGlobalMenu) {
+        myGlobalMenu.hidden = !myGlobalMenu.hidden;
+    }
+}
+@end
+
+// تشغيل التويك عند فتح اللعبة
 __attribute__((constructor)) static void initialize() {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        UIWindow *keyWindow = [[UIApplication sharedApplication] keyWindow];
-        MyMenuV *menu = [[MyMenuV alloc] initWithFrame:CGRectMake(50, 100, 250, 200)];
-        menu.hidden = YES;
-        [keyWindow addSubview:menu];
+        UIWindow *keyWindow = nil;
+        if (@available(iOS 13.0, *)) {
+            for (UIWindowScene *scene in [UIApplication sharedApplication].connectedScenes) {
+                if (scene.activationState == UISceneActivationStateForegroundActive) {
+                    keyWindow = scene.windows.firstObject;
+                    break;
+                }
+            }
+        } else {
+            keyWindow = [UIApplication sharedApplication].keyWindow;
+        }
 
-        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:nil action:nil];
-        tap.numberOfTouchesRequired = 3;
-        [tap addTarget:[getBlock block:^(id sender) { menu.hidden = !menu.hidden; }] action:@selector(invoke)];
-        [keyWindow addGestureRecognizer:tap];
-        NSLog(@"🚀 MOHAMMED MENU LOADED!");
+        if (keyWindow) {
+            myGlobalMenu = [[MyMenuV alloc] initWithFrame:CGRectMake(50, 100, 250, 200)];
+            myGlobalMenu.hidden = YES; // يبدأ مخفي
+            [keyWindow addSubview:myGlobalMenu];
+
+            UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:[MenuHandler class] action:@selector(handleTap)];
+            tap.numberOfTouchesRequired = 3; // 3 أصابع تفتح المنيو
+            [keyWindow addGestureRecognizer:tap];
+            
+            NSLog(@"🚀 MOHAMMED MENU LOADED SUCCESSFULLY!");
+        }
     });
 }

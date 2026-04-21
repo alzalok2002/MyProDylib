b80d0f062db638b17d0ac1e5d2a1d1465d8d3f66
alzalok2002 <alzalok2002@gmail.com>
2026-04-21 05:53:06 +0200

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -11,12 +11,10 @@
         self.layer.cornerRadius = 15;
         self.layer.borderWidth = 2;
         self.layer.borderColor = [UIColor greenColor].CGColor;
-        self.clipsToBounds = YES;
         
         UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 10, frame.size.width, 30)];
-        label.text = @"MOHAMMED PRO MENU";
+        label.text = @"MOHAMMED PRO";
         label.textColor = [UIColor whiteColor];
-        label.font = [UIFont boldSystemFontOfSize:14];
         label.textAlignment = NSTextAlignmentCenter;
         [self addSubview:label];
     }
@@ -24,27 +22,22 @@
 }
 @end
 
-static MyProMenu *mainMenu;
-
 static void initMenu() {
-    // انتظار 15 ثانية عشان اللعبة تستقر وما تكرش
+    // انتظار 15 ثانية عشان اللعبة تحمل ملفاتها وما تكرش
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(15 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         UIWindow *window = nil;
         
-        // الطريقة الحديثة والوحيدة المقبولة في iOS 15-18
-        if (@available(iOS 13.0, *)) {
-            for (UIWindowScene *scene in [UIApplication sharedApplication].connectedScenes) {
-                if (scene.activationState == UISceneActivationStateForegroundActive) {
-                    window = scene.windows.firstObject;
-                    break;
-                }
+        // الطريقة الوحيدة الآمنة لـ iOS 16/17/18
+        for (UIWindowScene *scene in [UIApplication sharedApplication].connectedScenes) {
+            if (scene.activationState == UISceneActivationStateForegroundActive) {
+                window = scene.windows.firstObject;
+                break;
             }
         }
 
-        if (window && !mainMenu) {
-            mainMenu = [[MyProMenu alloc] initWithFrame:CGRectMake(50, 100, 240, 160)];
-            [window addSubview:mainMenu];
-            NSLog(@"[MOHAMMED] Done! Menu is up.");
+        if (window) {
+            MyProMenu *menu = [[MyProMenu alloc] initWithFrame:CGRectMake(50, 100, 240, 160)];
+            [window addSubview:menu];
         }
     });
 }
